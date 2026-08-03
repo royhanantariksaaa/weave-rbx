e8aebfcaa74f7fa7619b8ad634615e72bdc5a49d
Royhan Antariksa <103055171+royhanantariksaaa@users.noreply.github.com>
2026-08-03 15:51:14 +0700

    Built and published by Moonwave

---
+++ assets/js/0c3c445d.95ad06d5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[953],{1794:e=>{e.exports=JSON.parse('[{"value":"Functions","id":"functions","level":2},{"value":":Destroy","id":"Destroy","level":3},{"value":":Get","id":"Get","level":3},{"value":":Peek","id":"Peek","level":3},{"value":":Set","id":"Set","level":3},{"value":":Update","id":"Update","level":3}]')}}]);
